8ac661ff0f571e70c6e97c6bc5c3ac2ce49e07b6
Ivaschenko Dmitri <tisomeke@proton.me>
2025-11-28 00:48:06 +0300

    blank theme toggle setting

--- app/src/main/java/ui/ProfileFragment.kt
+++ app/src/main/java/ui/ProfileFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.PopupMenu
 import com.example.tomatize.R
 
 class ProfileFragment : Fragment() {
@@ -13,6 +16,34 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        
+        val settingsIcon = view.findViewById<ImageView>(R.id.settings_icon)
+        settingsIcon.setOnClickListener {
+            showSettingsMenu(it)
+        }
+        
+        return view
+    }
+
+    private fun showSettingsMenu(view: View) {
+        val popup = PopupMenu(requireContext(), view)
+        popup.menu.add(0, 1, 0, "Toggle Theme")
+        
+        popup.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                1 -> {
+                    val currentMode = AppCompatDelegate.getDefaultNightMode()
+                    if (currentMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    } else {
+                         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
+        popup.show()
     }
 }
